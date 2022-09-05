dc42d270eac129760ad4a877d13aa2620474999b
Marcio <margeodev@gmail.com>
2022-09-05 19:02:32 -0300

    Adding base methods to category-form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Category } from '../shared/category.model';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  isEditing: boolean;
+  categoryForm: FormGroup;
+  pageTitle: string = 'Cadastro de Nova Categoria';
+  serverErrorMessages: string[] = null;
+  submitingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
+  }
+
+  ngAfterContentChecked(): void {
+    this.setPageTitle();
+  }
+
+
+  // PRIVATE METHODS
+
+  private setPageTitle() {
+    if(this.isEditing) {
+      const categoryName = this.category.name || ''
+      this.pageTitle = 'Editando Categoria: ' + categoryName;
+    }
+  }
+
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == "new") {
+      this.isEditing = false;
+    } else {
+      this.isEditing = true;
+    }
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if(this.isEditing) {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))
+      ).subscribe(category => {
+        this.category = category;
+        this.categoryForm.patchValue(this.category);
+      },
+      (error) => alert('Ocorreu um erro no servidor'))
+    }
   }
 
 }
