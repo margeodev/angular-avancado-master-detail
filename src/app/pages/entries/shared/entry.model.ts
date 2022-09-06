a93f108ecdadc0860bb3274224717fc2d015c346
Marcio <margeodev@gmail.com>
2022-09-06 15:49:29 -0300

    Adding entryList component

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -9,7 +9,7 @@ export class Entry {
         public amount?: string,
         public date?: string,
         public paid?: boolean,
-        public categoryId?: string,
+        public categoryId?: number,
         public category?: Category
     ) {}
 
